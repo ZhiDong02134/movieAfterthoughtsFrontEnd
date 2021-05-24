7cd72529544e33598776483e351d0349427a678f
Danny Dong <dannydong1870@gmail.com>
2021-05-24 02:03:25 -0400

    updated baseURL

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://zd-movie-afterthoughts.herokuapp.com/",
+  baseURL: "https://zd-movie-afterthoughts.herokuapp.com",
 });
